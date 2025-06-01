a59a25e7b4d9ada285b6961c4dceda70571928c0
김진영 <tweetyjinyoung@gmail.com>
2025-06-01 14:54:40 +0900

    김진영 step_03 숙제제출

--- step_03/solutions/kimjinyoung.js
+++ step_03/solutions/kimjinyoung.js
@@ -2,7 +2,17 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
   question.addEventListener("click", () => {
+
+    // 모든 답변 닫기
+    questions.forEach((q) => {
+      if (q !== question) {
+        q.classList.remove("active");
+        q.nextElementSibling.classList.remove("active");
+      }
+    });
+
     const answer = question.nextElementSibling;
+    question.classList.toggle("active");
     answer.classList.toggle("active");
   });
 });
