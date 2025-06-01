86d54152f33164db618b0f0a2de78311474cf52c
_H <_h@MacBookAir.local>
2025-06-01 09:52:20 +0900

    과제3 - 허정희 제출

---
+++ step_03/solutions/hurjunghee.js
@@ -0,0 +1,18 @@
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer");
+const icons = document.querySelectorAll(".icon");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    const icon = question.querySelector(".icon");
+    answers.forEach((answer) => {
+      answer.classList.remove("active");
+    });
+    icons.forEach((icon) => {
+      icon.textContent = "▶";
+    });
+    answer.classList.add("active");
+    icon.textContent = "▼";
+  });
+});
